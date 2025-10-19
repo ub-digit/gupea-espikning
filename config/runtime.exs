4cd5fefa85555e6287dd4c3d416b4f58d7f5733f
David Gustafsson <david.gustafsson@ub.gu.se>
2025-10-19 14:40:14 +0200

    Add endpoint url path environment variable

--- config/runtime.exs
+++ config/runtime.exs
@@ -62,13 +62,15 @@ if config_env() == :prod do
       You can generate one by calling: mix phx.gen.secret
       """
 
-  host = System.get_env("PHX_HOST") || "example.com"
+  url_host = System.get_env("PHX_HOST") || "example.com"
+  url_path = System.get_env("PHX_PATH") || "/espikning"
+
   port = String.to_integer(System.get_env("PORT") || "4000")
 
   config :espikning, :dns_cluster_query, System.get_env("DNS_CLUSTER_QUERY")
 
   config :espikning, EspikningWeb.Endpoint,
-    url: [host: host, port: 443, scheme: "https"],
+    url: [host: url_host, path: url_path, port: 443, scheme: "https"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
