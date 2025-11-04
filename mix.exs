d3a57de862ed0420c748070d8483d2a3217a2437
David Gustafsson <david.gustafsson@ub.gu.se>
2025-11-04 17:40:34 +0100

    Add gen_smpt dependecy explicitly as now optional in swoosh

--- mix.exs
+++ mix.exs
@@ -59,6 +59,7 @@ defmodule Espikning.MixProject do
        compile: false,
        depth: 1},
       {:swoosh, "~> 1.16"},
+      {:gen_smtp, "~> 1.2"},
       {:req, "~> 0.5"},
       {:telemetry_metrics, "~> 1.0"},
       {:telemetry_poller, "~> 1.0"},
